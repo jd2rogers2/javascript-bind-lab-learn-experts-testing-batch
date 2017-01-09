0d0a5c19cd8647f2eb70e98e439902699e0a769e
jd2rogers2 <jd2rogers2@gmail.com>
2017-01-09 16:07:29 -0500

    donzo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time : Math.round(Date.now() / 1000),
+  startTicking : function(){
+    setInterval(() => this.time ++, 1000)
+  }
+}
+
+function censor(word, sentence){
+  return sentence.replace(new RegExp(word, 'g'), 'BLEEP')
+}
+
+var violenceCensor = censor.bind(null, 'violence');
+
+var drugsCensor = censor.bind(null, 'drugs');
